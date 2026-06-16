cbff723a5c21861a4f5131de41a36a729a1c156d
Upender Arukonda <109522168+UArukonda@users.noreply.github.com>
2026-06-16 23:58:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let item = document.getElementById("level")
+let count = 0
+
+while(item.parentNode){
+	count++
+	item = item.parentNode
+}
+
+console.log(`The level of the element is: ${count}`)
